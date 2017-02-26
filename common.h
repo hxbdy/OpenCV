91c46dfcf4230342edf4247ce2c8235493a34bd5
hxbdy <hxbdy541@gmail.com>
2017-02-26 23:39:15 +0900

    henko

--- common.h
+++ common.h
@@ -3,6 +3,8 @@
 #pragma warning(disable : 4819)
 #include <opencv2\opencv.hpp>
 #pragma warning(default : 4819)
+#include <vector>
+#include <iostream>
 
 #ifdef _DEBUG
 #pragma comment(lib,"opencv_world310d.lib")
