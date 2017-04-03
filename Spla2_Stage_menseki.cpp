8ffef7365dd173f82359bb34975c51cce5b7ceea
hxbdy <hxbdy541@gmail.com>
2017-04-03 22:46:38 +0900

    Ver 1.0.0

--- Spla2_Stage_menseki.cpp
+++ Spla2_Stage_menseki.cpp
@@ -1,38 +1,37 @@
+//Splatoon2ステージのスクショからインク塗り面積を計算スルヨ
+//ステージ全体を100%としないで、総塗り面積を100%としてるヨ
+//実行方法はターミナルで以下の通りダヨ
+//OpenCV3.0 <ステージのマスク画像> <スクショ画像>
+//Ver 1.0.0
+
 #include "common.h"
-#define GRAY_LOW 0.7
-#define GRAY_UP  1.7
-#define GRAY_SUM 110
+#define GRAY_LOW 0.7 //BGR比の下範囲
+#define GRAY_UP  1.7 //BGR比の上範囲
+#define GRAY_SUM 110 //BGR総和の閾値
 
 using namespace cv;
 using namespace std;
 
 typedef struct Ink{
-	float per; //全体塗り面積の割合
-	float men;//塗り総面積
-	Mat   ink;//自陣インク色のみを反映させた画像生成
-	int   BGR[3];//元画像からの取得BGR
-	Scalar color[2];//自陣インク色
-	float H;//HSV
-	float HRange[2];//H範囲の下、上
+	float per;              //全体塗り面積の割合
+	float men;              //塗り総面積
+	Mat   ink;              //自陣インク色のみを反映させた画像生成
+	int   BGR[3];           //元画像からの取得BGR
+	Scalar color[2];        //自陣インク色
+	float H;                //HSVのH
+	float HRange[2];        //H範囲の下、上
 };
 
 //BGRをHSVのHに変換する
 float HSV_H(int BGR[]) {
-	float H, max, min;
+	float H;
+	int max, min;
 	max = MAX(MAX(BGR[0], BGR[1]), BGR[2]);
 	min = MIN(MIN(BGR[0], BGR[1]), BGR[2]);
-	if (max == BGR[0]) {
-		H = 60 * ((BGR[2] - BGR[1]) / (max - min)) + 240;
-	}
-	else if (max == BGR[1]) {
-		H = 60 * ((BGR[0] - BGR[2]) / (max - min)) + 120;
-	}
-	else if (max == BGR[2]) {
-		H = 60 * ((BGR[1] - BGR[0]) / (max - min));
-	}
-	if (H < 0) {
-		H += 360;
-	}
+	if (max == BGR[0]) H = float(60.0 * ((BGR[2] - BGR[1]) / (max - min)) + 240);
+	else if (max == BGR[1]) H = float(60.0 * ((BGR[0] - BGR[2]) / (max - min)) + 120);
+	else if (max == BGR[2]) H = float(60.0 * ((BGR[1] - BGR[0]) / (max - min)));
+	if (H < 0) H += 360.0;
 	return H;
 }
 
@@ -44,7 +43,7 @@ int main(int argc, char* argv[]) {
 		teki.men = 0;
 
 		int white = 0, cnt = 0,ink[3];
-		float min,R, G, B,H_low,H_up,H_thr;
+		float min,R, G, B,H_up,H_thr;
 		Mat stage,src1,src2,dst,dst2;
 		Mat teki_ink, mikata_ink;
 		
@@ -59,19 +58,14 @@ int main(int argc, char* argv[]) {
 		bitwise_and(src1, src2, dst);
 		cvtColor(dst, stage, COLOR_RGB2GRAY);
 		threshold(stage, stage, 80, 210, THRESH_BINARY);
-		imshow("dst", dst);
-		waitKey(0);
 		for (int y = 0; y < stage.rows; y++) {
 			for (int x = 0; x < stage.cols; x++) {
 				if (stage.at<unsigned char>(y, x)) white++;
 			}
 		}
-
 		mikata_ink = src1.clone();
 		teki_ink = src1.clone();
-		//imshow("mikata_hazie", mikata_ink);
-		//imshow("teki_hazime", teki_ink);
-		//waitKey(0);
+
 		//シュガー理論
 		//敵インクH
 		bool flg=true;
@@ -83,7 +77,7 @@ int main(int argc, char* argv[]) {
 				if (ink[0] == 0) ink[0] = 1;
 				if (ink[1] == 0) ink[1] = 1;
 				if (ink[2] == 0) ink[2] = 1;
-				min = MIN(MIN(ink[0], ink[1]), ink[2]);
+				min = float(MIN(MIN(ink[0], ink[1]), ink[2]));
 				B = ink[0] / min;
 				G = ink[1] / min;
 				R = ink[2] / min;
@@ -118,7 +112,6 @@ int main(int argc, char* argv[]) {
 			if (!flg) break;
 		}
 		cout << "teki BGR=" << ink[0] << " " << ink[1] << " " << ink[2]<<"\n";
-		//cout << "teki H=" << teki.H << "\n";
 
 		//味方インクH
 		flg = true;
@@ -130,11 +123,10 @@ int main(int argc, char* argv[]) {
 				if (ink[0] == 0) ink[0] = 1;
 				if (ink[1] == 0) ink[1] = 1;
 				if (ink[2] == 0) ink[2] = 1;
-				min = MIN(MIN(ink[0], ink[1]), ink[2]);
+				min = float(MIN(MIN(ink[0], ink[1]), ink[2]));
 				B = ink[0] / min;
 				G = ink[1] / min;
 				R = ink[2] / min;
-				//cout << B << " "<< G << " "<<R <<"\n";
 				if (ink[0] + ink[1] + ink[2] > GRAY_SUM) {
 					if (B == 1.0) {
 						if ((GRAY_LOW > G || GRAY_UP < G) || (GRAY_LOW > R || GRAY_UP < R)) {
@@ -172,7 +164,6 @@ int main(int argc, char* argv[]) {
 		//インク閾値計算
 		H_thr = (mikata.H + teki.H)/2;
 		H_up =H_thr + 180;
-		//if (H_up == 360) H_up = 0;
 		cout << "H_low=" << H_thr << " " << "H_up=" << H_up << "\n";
 		if (H_thr <= teki.H && teki.H <= H_up) {
 			teki.HRange[0] = H_thr;
@@ -200,12 +191,11 @@ int main(int argc, char* argv[]) {
 				if (ink[0] == 0) ink[0] = 1;
 				if (ink[1] == 0) ink[1] = 1;
 				if (ink[2] == 0) ink[2] = 1;
-				min = MIN(MIN(ink[0], ink[1]), ink[2]);
+				min = float(MIN(MIN(ink[0], ink[1]), ink[2]));
 				B = ink[0] / min;
 				G = ink[1] / min;
 				R = ink[2] / min;
 				if (ink[0] + ink[1] + ink[2] > GRAY_SUM) {
-					//cout << B << " "<< G << " "<<R <<"\n";
 					if (B == 1.0) {
 						if ((GRAY_LOW > G || GRAY_UP < G) || (GRAY_LOW > R || GRAY_UP < R)) {
 							if (mikata.HRange[0] <= HSV_H(ink) && HSV_H(ink) <= mikata.HRange[1] ||
@@ -268,8 +258,6 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		//cout << "teki range:" << teki.HRange[0] << " - " << teki.HRange[1] << "\n";
-		//cout << "mikata range:" << mikata.HRange[0] << " - " << mikata.HRange[1] << "\n";
 		imshow("teki_ink", teki_ink);
 		imshow("mikata_ink", mikata_ink);
 		imwrite("ink2.png", teki_ink);
